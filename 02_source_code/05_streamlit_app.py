1ee3099c91fae44b6f8307be1332005816383494
unknown <manshika827@gmail.com>
2026-07-19 01:31:06 +0530

    Fix Streamlit app

--- 02_source_code/05_streamlit_app.py
+++ 02_source_code/05_streamlit_app.py
@@ -1,8 +1,13 @@
+import os
 import re
+import joblib
+import streamlit as st
 import nltk
+
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 
+# Download NLTK stopwords (only first time)
 nltk.download("stopwords")
 
 stemmer = PorterStemmer()
@@ -21,44 +26,26 @@ def clean_text(text):
             words.append(stemmer.stem(word))
 
     return " ".join(words)
+
+
 # Project path
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-model_path = os.path.join(
-    BASE_DIR,
-    "03_models",
-    "fake_news_model.pkl"
-)
-
-vectorizer_path = os.path.join(
-    BASE_DIR,
-    "03_models",
-    "tfidf_vectorizer.pkl"
-)
-
+model_path = os.path.join(BASE_DIR, "03_models", "fake_news_model.pkl")
+vectorizer_path = os.path.join(BASE_DIR, "03_models", "tfidf_vectorizer.pkl")
 
 # Load model
 model = joblib.load(model_path)
 vectorizer = joblib.load(vectorizer_path)
 
-
-# Page title
+# UI
 st.title("📰 Fake News Detection System")
+st.write("Enter any news article and the AI model will classify it.")
 
-st.write(
-    "Enter any news text and the AI model will classify it."
-)
-
-
-# Input box
-news = st.text_area(
-    "Enter News Article"
-)
-
+news = st.text_area("Enter News Article")
 
 if st.button("Predict"):
-    
+
     if news.strip() == "":
         st.warning("Please enter news text")
 
